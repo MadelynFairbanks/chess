047d3846850c7ed15c98ddd4196e8610df234484
MadelynFairbanks <aquadolphin716@gmail.com>
2025-05-20 19:26:23 -0600

    Add clear endpoint functionality

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,4 +1,75 @@
 package dataaccess;
 
-public class MemoryDataAccess {
+import model.UserData;
+import model.AuthData;
+import model.GameData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryDataAccess implements DataAccess {
+
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private int nextGameID = 1;
+
+    @Override
+    public void clear() {
+        users.clear();
+        authTokens.clear();
+        games.clear();
+        nextGameID = 1;
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists.");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return authTokens.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        authTokens.remove(authToken);
+    }
+
+    @Override
+    public void createGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game does not exist.");
+        }
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData[] listGames() {
+        return games.values().toArray(new GameData[0]);
+    }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,20 @@
 package server;
 
 import spark.*;
+import dataaccess.MemoryDataAccess;
+import service.ClearService;
 
 public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
+
+        MemoryDataAccess memoryData = new MemoryDataAccess();
+        ClearService clearService = new ClearService(memoryData);
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
+        Spark.delete("/db", clearHandler);
 
         // Register your endpoints and handle exceptions here.
 
